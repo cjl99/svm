1ab911ef8ef7b37f82e28b01908bc1f7cf6bd83d
cjl99 <cjl2559@163.com>
2023-05-19 15:22:47 +0800

    additional model

--- models.py
+++ models.py
@@ -151,3 +151,83 @@ class Dual_Soft_SVM(Primal_Soft_SVM):
 
         return self
 
+class Addition_Primal(Primal_Soft_SVM):
+    def __init__(self, C) -> None:
+        super().__init__(C)
+
+    def fit(self, X, y):
+        n, d = X.shape[0], X.shape[1]
+
+        P = np.zeros((n+d+1, n+d+1))  # shape (n+d+1, n+d+1)
+        P[:d, :d] = np.eye(d)  
+        q = np.zeros((n+d+1, ))  # shape (n+d+1, )
+        # modify here 
+        q[d+1:] = self.C / n * np.ones((n, )) 
+
+        G = np.zeros((2*n, n+d+1))  # shape (2*n, n+d+1)
+        G[:n, :d] = -y.reshape(-1, 1) * X # broadcast & pointwise product
+        G[:n, d] = y  # here is different 
+        G[:n, d+1:] = -np.eye(n)
+        G[n:, d+1:] = -np.eye(n)
+
+        h = np.zeros((2*n, ))  # shape (2*n, )
+        h[:n] = -1.0 
+
+        sol = solvers.qp(matrix(P), matrix(q), matrix(G), matrix(h))
+        self.w = np.array(sol['x']).flatten()[:d+1]  
+        self.xi = np.array(sol['x']).flatten()[d+1:]  
+
+        return self    
+
+    def predict(self, X):
+        """Predict given data, which can be used for all SVM Model."""
+        n = X.shape[0]
+        y_values = np.concatenate((X, np.ones((n, 1))*-1), axis=1) @ self.w  # (n, d+1) @ (d+1, ) = (n, )
+        y_labels = np.sign(y_values)  # (n, )
+        
+        return y_values, y_labels
+
+class Addition_Dual(Primal_Soft_SVM):
+    def __init__(self, C) -> None:
+        super().__init__(C)
+    
+    def fit(self, X, y):
+        n, d = X.shape
+
+        P = np.zeros((n, n), dtype=float)  # shape (n, n)
+        for i in range(n):
+            for j in range(n):
+                P[i, j] = y[i] * y[j] * X[i] @ X[j].reshape(-1, 1)  # (1, d) @ (d, 1) = (1, 1)
+        
+        q = -1 * np.ones((n, ))  # shape (n, )
+    
+        G = np.concatenate((-np.eye(n), np.eye(n)), axis=0)  # shape (2*n, n)
+        h = np.concatenate((np.zeros((n, )), self.C / n * np.ones((n, ))), axis=0)  # modify here 
+        A = y.reshape(1, -1)  # shape (1, n)
+        b = 0.
+
+        sol = solvers.qp(matrix(P), matrix(q), matrix(G), matrix(h), matrix(A), matrix(b))
+        self.alpha = np.array(sol['x']).flatten()
+        
+        self.w = np.zeros((d, ))
+        for i in range(n):
+            self.w += self.alpha[i] * y[i] * X[i] # (1, ) * (1, ) * (1, d) = (d, ) 
+
+        cnt = 0
+        for i in range(n):
+            if self.alpha[i] > 1e-8:
+                cnt += 1
+                b += X[i] @ self.w - y[i]
+        b /= cnt
+
+        self.w = np.concatenate((self.w, np.array([b])), axis=0)  # (d, ) -> (d+1, )
+
+        return self
+    
+    def predict(self, X):
+        """Predict given data, which can be used for all SVM Model."""
+        n = X.shape[0]
+        y_values = np.concatenate((X, np.ones((n, 1))*-1), axis=1) @ self.w  # (n, d+1) @ (d+1, ) = (n, )
+        y_labels = np.sign(y_values)  # (n, )
+        
+        return y_values, y_labels
--- solvesvm.py
+++ solvesvm.py
@@ -1,9 +1,41 @@
 import os
 import numpy as np
 import pandas as pd
-from models import Primal_Hard_SVM, Dual_Hard_SVM, Primal_Soft_SVM, Dual_Soft_SVM
+from models import Primal_Hard_SVM, Dual_Hard_SVM, Primal_Soft_SVM, Dual_Soft_SVM, Addition_Primal, Addition_Dual
 from libsvm.svmutil import *
 import argparse
+
+
+def compare_weigts(primal, dual, libsvm):
+    "Compute L2 distance between primal, dual and libsvm weights."
+    assert primal.shape == dual.shape == libsvm.shape, "weights should have the same shape."
+    primal_dist = np.linalg.norm(primal - libsvm)
+    dual_dist = np.linalg.norm(dual - libsvm)
+    primal_dual = np.linalg.norm(primal - dual)
+
+    print(f"distance between primal weights and dual weights: {primal_dual} ")
+    print(f"distance between primal weights and libsvm weights: {primal_dist} ")
+    print(f"distance between dual weights and libsvm weights: {dual_dist}")
+
+
+def read_datasets(file_path: str):
+    df = pd.read_csv(file_path)
+    X = df.values[:, 3:].astype(float)
+    y = np.array([1 if i == "M" else -1 for i in df.values[:, 2]]).astype(float)
+
+    assert X.shape[0] == y.shape[0], "X and y should have the same number of rows."
+    return X, y
+    
+def split_data(X, y):
+    ratio = 0.7
+    n = X.shape[0]
+    n_train = int(n * ratio)
+
+    return X[:n_train], y[:n_train], X[n_train:], y[n_train:]
+
+def calcuate_acc(y_true, y_pred):
+    return np.sum(y_true == y_pred) / len(y_true)
+    
 
 def run_easy_hard_test():
     X = np.array([[3,3], [4,3], [1,1]], dtype=float)
@@ -88,45 +120,41 @@ def run_data_a1_soft_test(C: float = 1.5):
     print(f"dual soft-margin svm acc: {dual_soft_acc}")
     print(f"libsvm acc: {libsvm_acc}")
 
-def compare_weigts(primal, dual, libsvm):
-    "Compute L2 distance between primal, dual and libsvm weights."
-    assert primal.shape == dual.shape == libsvm.shape, "weights should have the same shape."
-    primal_dist = np.linalg.norm(primal - libsvm)
-    dual_dist = np.linalg.norm(dual - libsvm)
-    primal_dual = np.linalg.norm(primal - dual)
 
-    print(f"distance between primal weights and dual weights: {primal_dual} ")
-    print(f"distance between primal weights and libsvm weights: {primal_dist} ")
-    print(f"distance between dual weights and libsvm weights: {dual_dist}")
+def run_addition_test(C:float = 1.5):
+    # easy test
+    X, y = read_datasets("./data_a1/data.csv")
+    n, d = X.shape
+    print(f"There are {n} samples and {d} features in the dataset.")
 
+    X, y, test_data, y_true = split_data(X, y)
 
-def read_datasets(file_path: str):
-    df = pd.read_csv(file_path)
-    X = df.values[:, 3:].astype(float)
-    y = np.array([1 if i == "M" else -1 for i in df.values[:, 2]]).astype(float)
+    # primal soft-margin svm -- addition version
+    addi_primal = Addition_Primal(C).fit(X, y)
+    print("addition primal soft-margin svm coefs: ", addi_primal.w)
+    y_value, y_pred = addi_primal.predict(test_data)
+    print(f"addition primal soft-margin svm acc: {calcuate_acc(y_true, y_pred)}")
 
-    assert X.shape[0] == y.shape[0], "X and y should have the same number of rows."
-    return X, y
-    
-def split_data(X, y):
-    ratio = 0.7
-    n = X.shape[0]
-    n_train = int(n * ratio)
+    # dual soft-margin svm -- addition version
+    addi_dual = Addition_Dual(C).fit(X, y)
+    print("addition dual soft-margin svm coefs:", addi_dual.w)
+    y_value, y_pred = addi_dual.predict(test_data)
+    print(f"addition dual soft-margin svm acc: {calcuate_acc(y_true, y_pred)}")
 
-    return X[:n_train], y[:n_train], X[n_train:], y[n_train:]
+    compare_weigts(addi_primal.w, addi_dual.w, np.zeros((28,)))
 
-def calcuate_acc(y_true, y_pred):
-    return np.sum(y_true == y_pred) / len(y_true)
-    
 
 if __name__ == "__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--C", type=float, default=1.5)
     args = args.parse_args()
-    # run_easy_hard_test()
-    # run_easy_soft_test()
+
+    run_easy_hard_test()
+    run_easy_soft_test()
 
     run_data_a1_soft_test(C = args.C)
+
+    run_addition_test(C = args.C) 
 
 
 
